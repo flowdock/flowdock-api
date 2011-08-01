263e241929e910e1b79dcb746bfcef5050369436
Antti Pitkanen <antti.pitkanen@nodeta.fi>
2011-08-01 13:27:37 +0300

    Refactored API to be more RESTful and include :format parameter for message

--- lib/flowdock-api.rb
+++ lib/flowdock-api.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'httparty'
 
 module Flowdock
-  FLOWDOCK_API_URL = "http://api.local.nodeta.dmz/v1/influx"
+  FLOWDOCK_API_URL = "http://api.local.nodeta.dmz/v1/messages/influx"
 
   class << self
     def new(options={})
@@ -18,26 +18,28 @@ module Flowdock
     class InvalidMessageError < StandardError; end
 
     def initialize(api_token, source, from)
-      raise ApiTokenMissingError if api_token.blank?
+      raise ApiTokenMissingError, "Flow must have :api_token attribute" if api_token.blank?
       @api_token = api_token
       
-      raise InvalidSourceError if source.blank? || !source.match(/^\w+$/i)
+      raise InvalidSourceError, "Flow must have :source attribute" if source.blank? || !source.match(/^\w+$/i)
       @source = source
 
-      raise InvalidSenderInformationError if from.nil? || !from.kind_of?(Hash)
+      raise InvalidSenderInformationError, "Flow must have :from attribute" if from.nil? || !from.kind_of?(Hash)
       from.reject! { |k,v| ![:name, :address].include?(k) }
-      raise InvalidSenderInformationError if from[:name].blank? || from[:address].blank?
+      raise InvalidSenderInformationError, "Flow's :from attribute must have both :name and :address" if from[:name].blank? || from[:address].blank?
       @from = from
     end
 
     def send_message(params)
-      raise InvalidMessageError if params[:subject].blank? || params[:content].blank?
+      raise InvalidMessageError, "Message must have both :subject and :content" if params[:subject].blank? || params[:content].blank?
+      raise InvalidMessageError, "Message must have :format with one of following values: html" if params[:format].blank? || !["html"].include?(params[:format])
 
       tags = (params[:tags].kind_of?(Array)) ? params[:tags] : []
       tags.reject! { |tag| !tag.kind_of?(String) || tag.blank? }
       
-      resp = self.class.post(get_flowdock_api_url_for("send_message"), :body => {
+      resp = self.class.post(get_flowdock_api_url, :body => {
           :source => @source,
+          :format => params[:format],
           :from_name => @from[:name],
           :from_address => @from[:address],
           :subject => params[:subject],
@@ -50,8 +52,8 @@ module Flowdock
   
     private
   
-    def get_flowdock_api_url_for(method)
-      "#{FLOWDOCK_API_URL}/#{@api_token}/#{method}"
+    def get_flowdock_api_url
+      "#{FLOWDOCK_API_URL}/#{@api_token}"
     end
   end
 end
--- spec/flowdock_api_spec.rb
+++ spec/flowdock_api_spec.rb
@@ -2,47 +2,47 @@ require 'spec_helper'
 require 'fakeweb'
 
 describe Flowdock do
-  before(:each) do
-  end
   
-  it "should initialize flow with given token and sender information" do
-    lambda {
-      @flow = Flowdock.new(:api_token => "test", :source => "myapp", 
-        :from => {:name => "test", :address => "invalid@nodeta.fi"})
-    }.should_not raise_error
-  end
+  describe "with initializing flow" do
+    it "should succeed with correct token and sender information" do
+      lambda {
+        @flow = Flowdock.new(:api_token => "test", :source => "myapp", 
+          :from => {:name => "test", :address => "invalid@nodeta.fi"})
+      }.should_not raise_error
+    end
   
-  it "should fail to initialize flow without token" do
-    lambda {
-      @flow = Flowdock.new(:api_token => "", :source => "myapp", 
-        :from => {:name => "test", :address => "invalid@nodeta.fi"})
-    }.should raise_error(Flowdock::Flow::ApiTokenMissingError)
-  end
+    it "should fail without token" do
+      lambda {
+        @flow = Flowdock.new(:api_token => "", :source => "myapp", 
+          :from => {:name => "test", :address => "invalid@nodeta.fi"})
+      }.should raise_error(Flowdock::Flow::ApiTokenMissingError)
+    end
   
-  it "should fail to initialize flow without source" do
-    lambda {
-      @flow = Flowdock.new(:api_token => "test", :source => "",
-       :from => {:name => "test", :address => "invalid@nodeta.fi"})
-    }.should raise_error(Flowdock::Flow::InvalidSourceError)
-  end
+    it "should fail without source" do
+      lambda {
+        @flow = Flowdock.new(:api_token => "test", :source => "",
+         :from => {:name => "test", :address => "invalid@nodeta.fi"})
+      }.should raise_error(Flowdock::Flow::InvalidSourceError)
+    end
   
-  it "should fail to initialize flow when source is not alphanumeric" do
-    lambda {
-      @flow = Flowdock.new(:api_token => "test", :source => "$foobar",
-       :from => {:name => "test", :address => "invalid@nodeta.fi"})
-    }.should raise_error(Flowdock::Flow::InvalidSourceError)
-  end
+    it "should fail when source is not alphanumeric" do
+      lambda {
+        @flow = Flowdock.new(:api_token => "test", :source => "$foobar",
+         :from => {:name => "test", :address => "invalid@nodeta.fi"})
+      }.should raise_error(Flowdock::Flow::InvalidSourceError)
+    end
   
-  it "should fail to initialize flow without sender information" do
-    lambda {
-      @flow = Flowdock.new(:api_token => "test", :source => "myapp",
-       :from => {:name => "", :address => "invalid@nodeta.fi"})
-    }.should raise_error(Flowdock::Flow::InvalidSenderInformationError)
+    it "should fail without sender information" do
+      lambda {
+        @flow = Flowdock.new(:api_token => "test", :source => "myapp",
+         :from => {:name => "", :address => "invalid@nodeta.fi"})
+      }.should raise_error(Flowdock::Flow::InvalidSenderInformationError)
     
-    lambda {
-      @flow = Flowdock.new(:api_token => "test", :source => "myapp",
-       :from => {:name => "test", :address => ""})
-    }.should raise_error(Flowdock::Flow::InvalidSenderInformationError)
+      lambda {
+        @flow = Flowdock.new(:api_token => "test", :source => "myapp",
+         :from => {:name => "test", :address => ""})
+      }.should raise_error(Flowdock::Flow::InvalidSenderInformationError)
+    end
   end
   
   describe "with sending messages" do
@@ -56,31 +56,37 @@ describe Flowdock do
     
     it "should not send without subject" do
       lambda {
-        @flow.send_message(:subject => "", :content => "Test")
+        @flow.send_message(:subject => "", :content => "Test", :format => "html")
       }.should raise_error(Flowdock::Flow::InvalidMessageError)
     end
     
     it "should not send without content" do
       lambda {
-        @flow.send_message(:subject => "", :content => "Test")
+        @flow.send_message(:subject => "Test", :content => "", :format => "html")
+      }.should raise_error(Flowdock::Flow::InvalidMessageError)
+    end
+    
+    it "should not send without correct format" do
+      lambda {
+        @flow.send_message(:subject => "Testing", :content => "Test", :format => "")
       }.should raise_error(Flowdock::Flow::InvalidMessageError)
     end
     
     it "should send with valid parameters and return true" do
       lambda {
         FakeWeb.register_uri(:post,
-                             "#{Flowdock::FLOWDOCK_API_URL}/#{@token}/send_message",
+                             "#{Flowdock::FLOWDOCK_API_URL}/#{@token}",
                              :status => ["200", "OK"], :body => "")
-        @flow.send_message(:subject => "Test", :content => "Test", :tags => ["cool", "stuff"]).should be_true
+        @flow.send_message(:subject => "Test", :content => "Test", :format => "html", :tags => ["cool", "stuff"]).should be_true
       }.should_not raise_error
     end
     
     it "should return false if backend returns anything but 200 OK" do
       lambda {
         FakeWeb.register_uri(:post,
-                             "#{Flowdock::FLOWDOCK_API_URL}/#{@token}/send_message",
+                             "#{Flowdock::FLOWDOCK_API_URL}/#{@token}",
                              :status => ["500", "Internal Server Error"], :body => "")
-        @flow.send_message(:subject => "Test", :content => "Test", :tags => ["cool", "stuff"]).should be_false
+        @flow.send_message(:subject => "Test", :content => "Test", :format => "html", :tags => ["cool", "stuff"]).should be_false
       }.should_not raise_error
     end
     
