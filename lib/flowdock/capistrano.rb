b3670b87b26fad6d6555f870eec5ae7e962db8fa
Antti Pitkanen <antti.pitkanen@nodeta.fi>
2012-02-09 13:41:42 +0200

    Working deployment error messages

--- lib/flowdock/capistrano.rb
+++ lib/flowdock/capistrano.rb
@@ -15,7 +15,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       begin
         run "echo '#{source.head}' > #{current_path}/BRANCH"
       rescue => e
-        puts "Flowdock: error in saving deployed branch information: " + e
+        puts "Flowdock: error in saving deployed branch information: #{e.to_s}"
       end
     end
 
@@ -25,7 +25,7 @@ Capistrano::Configuration.instance(:must_exist).load do
         set :repo, Grit::Repo.new(".")
         config = Grit::Config.new(repo)
       rescue => e
-        puts "Flowdock: error in fetching your git repository information: " + e
+        puts "Flowdock: error in fetching your git repository information: #{e.to_s}"
       end
 
       begin
@@ -33,7 +33,7 @@ Capistrano::Configuration.instance(:must_exist).load do
           :source => "Capistrano deployment", :project => flowdock_project_name,
           :from => {:name => config["user.name"], :address => config["user.email"]})
       rescue => e
-        puts "Flowdock: error in configuring Flowdock API: " + e
+        puts "Flowdock: error in configuring Flowdock API: #{e.to_s}"
       end
     end
 
@@ -45,7 +45,7 @@ Capistrano::Configuration.instance(:must_exist).load do
           :content => notification_message,
           :tags => ["deploy", "#{rails_env}"] | flowdock_deploy_tags)
       rescue => e
-        puts "Flowdock: error in sending notification to your flow: " + e
+        puts "Flowdock: error in sending notification to your flow: #{e.to_s}"
       end
     end
 
